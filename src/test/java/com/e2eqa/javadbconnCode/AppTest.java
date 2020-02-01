9d1bbbac47705db7f61a7b00c57e70a3d5be4c5e
Fawad Malik <fawad.malik@hotmail.com>
2020-01-31 23:55:03 -0800

    Adds new repository shell code

---
+++ src/test/java/com/e2eqa/javadbconnCode/AppTest.java
@@ -0,0 +1,20 @@
+package com.e2eqa.javadbconnCode;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
